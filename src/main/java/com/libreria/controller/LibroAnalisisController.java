0d421bb1d56fd640c6baecd2c16b775eb074af77
Gwynblaidd <tomasBarone@users.noreply.github.com>
2026-07-02 23:10:28 -0300

    feat: Agregar Controller, Service y Repository para la entidad LibroAnalisis

---
+++ src/main/java/com/libreria/controller/LibroAnalisisController.java
@@ -0,0 +1,21 @@
+package com.libreria.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.libreria.model.LibroAnalisis;
+
+@RestController
+@RequestMapping("/api/analisis")
+public class LibroAnalisisController {
+
+	
+	public ResponseEntity<LibroAnalisis> obtenerInformacion(Long id){
+		
+		
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/libreria/service/LibroAnalisisService.java
@@ -0,0 +1,8 @@
+package com.libreria.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibroAnalisisService {
+
+}
